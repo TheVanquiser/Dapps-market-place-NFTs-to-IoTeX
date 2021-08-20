dd3b861061d87ecf9086844eaa0e16ff99c94a2c
TheVanquiser <86550334+TheVanquiser@users.noreply.github.com>
2021-08-21 03:44:07 +0700

    Add files via upload

---
+++ src/config.js
@@ -0,0 +1,30 @@
+// Config
+
+const config = {
+	CHAINID      : 1,
+	CHAINTYPE    : 'IoTeX',
+	NETWORK      : 'mainnet',
+	NETURL       : 'https://babel-api.mainnet.iotex.io',
+	GATEWAY      : 'https://ipfs.io/ipfs/',
+	EXPLORER     : 'https://iotexscan.io',
+	GASPRICE     : 10000000000000,
+	GASLIMIT     :   10000,
+	BuyerFees    : 2,  // 2%
+	SellerFees   : 2,  // 2%
+	AuthorFees   : 100, // 100 IOTX
+	Market       : '',
+	Auctions     : '',
+	Operator     : '',
+	TransferProxy: '',
+	Collections  : {
+		image : '',
+		model : '',
+		audio : '',
+		music : '',
+		video : '',
+		movie : '',
+		book  : '',
+		domain: '',
+		text  : ''
+	}
+}
